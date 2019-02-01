78f4ca7b3b33bcd44e585445d1565b2bcc3d8ded
davidzabalperez <davidzabalperez@gmail.com>
2019-02-01 13:53:31 +0100

    api con token

--- routes/api.php
+++ routes/api.php
@@ -5,10 +5,4 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::apiResource('/dogs','APIRestController');
-
-Route::middleware('auth:api')->group( function () {
-
-	Route::resource('products', 'API\ProductController');
-
-});
+Route::apiResource('/dogs','APIRestController')->middleware('auth:api');
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,26 @@
 }); */
 Route::get('/','ConsumeApiController@getStarsWars');
 Route::resource('imgur','ImgurController');
+
+Route::get('/callback', function (Request $request) {
+    $http = new GuzzleHttp\Client;
+
+    $response = $http->post('http://your-app.com/oauth/token', [
+        'form_params' => [
+            'grant_type' => 'authorization_code',
+            'client_id' => 'client-id',
+            'client_secret' => 'client-secret',
+            'redirect_uri' => 'http://example.com/callback',
+            'code' => $request->code,
+        ],
+    ]);
+
+    return json_decode((string) $response->getBody(), true);
+});
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
